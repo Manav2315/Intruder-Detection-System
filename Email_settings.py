9c30829edbbccb3d003f65d9cd5be159eedb8954
Manav More <130427574+Manav2315@users.noreply.github.com>
2024-07-12 13:22:04 +0530

    Update Email_settings.py

--- Email_settings.py
+++ Email_settings.py
@@ -1,3 +1,3 @@
-password = "azxr vgzv nipa blvq"
-from_email = "damofawesome7@gmail.com"  # must match the email used to generate the password
-to_email = "mmore2329@gmail.com"  # receiver email
+password = ""
+from_email = ""  # must match the email used to generate the password
+to_email = ""  # receiver email
